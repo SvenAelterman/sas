f53b9a134f60009853d5427e4b4cfc52d0a6a572
Sven Aelterman <17446043+SvenAelterman@users.noreply.github.com>
2022-02-10 10:33:34 -0500

    Remove importing useEffect

--- src/sas.front/src/components/Selector/Selector.js
+++ src/sas.front/src/components/Selector/Selector.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import PropTypes from 'prop-types'
 import InputLabel from "@mui/material/InputLabel"
 import FormControl from "@mui/material/FormControl"
